c50238b22ec7c47bdf26f83dad239f09e1f0e3a4
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-29 22:07:32 -0300

    Created a DependencyInjections to connect with the database

---
+++ HealthConnect.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Infrastructure;
+
+public static class DependencyInjection
+{
+
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentException("Connection string 'DefaultConnection' is not configured.");
+        }
+
+        services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+
+        return services;
+    }
+
+}
