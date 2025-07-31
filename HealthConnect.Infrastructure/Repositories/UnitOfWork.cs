4ae923b26025478167e4f27c39b4a39678b549f9
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-07-30 23:10:14 -0300

    Created a Repository for UnitOfWork

---
+++ HealthConnect.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using HealthConnect.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Infrastructure.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _appDbContext;
+    public UnitOfWork(AppDbContext context)
+    {
+        _appDbContext = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _appDbContext.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _appDbContext.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+}
