a0379c9c3b5f4c6458c2aab19b949c654e3b3221
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-03 14:33:54 -0300

    feat: Created a new DependencyInjection.cs to organize program's functions

---
+++ HealthConnect.Api/DependencyInjection.cs
@@ -0,0 +1,52 @@
+﻿using HealthConnect.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HealthConnect.Api;
+
+[ExcludeFromCodeCoverage]
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowWebApp",
+                policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+        });
+
+        return services;
+    }
+
+    public static WebApplication UsePresentation(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var DbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            DbContext.Database.Migrate();
+        }
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseCors("AllowWebApp");
+        app.UseAuthorization();
+        app.MapControllers();
+
+        return app;
+    }
+}
