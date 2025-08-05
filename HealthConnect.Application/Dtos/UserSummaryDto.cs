20f80ac257542367fd66293b50d946b1d23cc3c1
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-04 23:09:53 -0300

    feat: Created interface for UserRepository

---
+++ HealthConnect.Application/Dtos/UserSummaryDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Application.Dtos;
+
+public record UserSummaryDto
+{
+    public Guid Id { get; }
+
+    public string Name { get; }
+
+    public string Email { get; }
+
+    public string CPF { get; }
+
+    public DateOnly BirthDate { get; }
+
+}
---
+++ HealthConnect.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using HealthConnect.Domain.Models;
+namespace HealthConnect.Application.Interfaces;
+
+public interface IUserRepository
+{
+    public Task<IEnumerable<User>> GetAllUsers();
+
+    public Task<User?> GetUserById(Guid Id);
+
+    public Task<User?> GetUserByEmail(string Email);
+
+    public Task AddUser(User User);
+
+}
