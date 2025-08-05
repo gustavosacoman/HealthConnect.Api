032732c31e834f477c2beff3eb315ef780b32e34
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-04 23:01:16 -0300

    feat: Completed User Model

--- HealthConnect.Domain/Models/User.cs
+++ HealthConnect.Domain/Models/User.cs
@@ -11,7 +11,17 @@ public class User : IAuditable
 {
     public Guid Id { get; set; }
 
+    public string Name { get; set; }
+
     public string Email { get; set; }
+
+    public string CPF { get; set; }
+
+    public string hashedPassword { get; set; }
+
+    public string salt { get; set; }
+
+    public DateOnly BirthDate { get; set; }
 
     public DateTime CreatedAt { get; set; }
 
