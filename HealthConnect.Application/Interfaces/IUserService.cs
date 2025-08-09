67a205e2e888d4e3bb06e4de3e8ce142af8a2d24
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-09 14:55:12 -0300

    feat: created interface for Service

---
+++ HealthConnect.Application/Interfaces/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using HealthConnect.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Application.Interfaces;
+
+public interface IUserService
+{
+    public Task<UserSummaryDto> GetUserById(Guid id);
+
+    public Task<UserSummaryDto> GetUserByEmail(string email);
+
+    public Task<IEnumerable<UserSummaryDto>> GetAllUsers();
+
+    public Task<UserSummaryDto> CreateUser(UserRegistrationDto data);
+
+    public Task<UserSummaryDto> UpdateUser(Guid Id, UserUpdatingDto data);
+
+    public Task DeleteUser(Guid Id);
+}
