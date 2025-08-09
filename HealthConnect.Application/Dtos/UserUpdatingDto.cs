a5cb4393bc81183cecdfbd328b83b33765052850
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-08 22:26:53 -0300

    feat: Created user Dtos

---
+++ HealthConnect.Application/Dtos/UserUpdatingDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Application.Dtos;
+
+public record UserUpdatingDto
+{
+    public string? Name { get; init; }
+
+    public string? Email { get; init; }
+
+    public string? Password { get; init; }
+
+    public string? CPF { get; init; }
+
+    public DateOnly? BirthDate { get; init; }
+}
