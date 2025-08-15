15fd849f2fe0f8dff7960d8e8f390716737131d1
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-14 23:20:49 -0300

    feat: documented code and removed unnecessary usings

--- HealthConnect.Application/Interfaces/IPasswordHasher.cs
+++ HealthConnect.Application/Interfaces/IPasswordHasher.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace HealthConnect.Application.Interfaces;
 
-namespace HealthConnect.Application.Interfaces;
-
+/// <summary>
+/// Provides methods for generating salts and hashing passwords.
+/// </summary>
 public interface IPasswordHasher
 {
+    /// <summary>
+    /// Generates a cryptographically secure salt for password hashing.
+    /// </summary>
+    /// <returns>A base64-encoded salt string.</returns>
     string GenerateSalt();
+
+    /// <summary>
+    /// Hashes the specified password using the provided salt.
+    /// </summary>
+    /// <param name="password">The password to hash.</param>
+    /// <param name="salt">The salt to use for hashing.</param>
+    /// <returns>The hashed password as a string.</returns>
     string HashPassword(string password, string salt);
 }
