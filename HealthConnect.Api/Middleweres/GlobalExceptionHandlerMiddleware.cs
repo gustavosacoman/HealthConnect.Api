f5b8811372f231f7fc705a82f20e16ace7f58bfe
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-16 17:30:36 -0300

    feat: made middleware Exception a handler de catch-all

---
+++ HealthConnect.Api/Middleweres/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+namespace HealthConnect.Api.Middleweres;
+
+public class GlobalExceptionHandlerMiddleware(
+    RequestDelegate next,
+    ILogger<GlobalExceptionHandlerMiddleware> logger)
+{
+    private readonly RequestDelegate _next = next;
+    private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger = logger;
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unhandled exception occurred while processing the request.");
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var statusCode = exception switch
+        {
+            KeyNotFoundException _ => HttpStatusCode.NotFound,
+            ArgumentException _ => HttpStatusCode.BadRequest,
+            InvalidOperationException _ => HttpStatusCode.BadRequest,
+            _ => HttpStatusCode.InternalServerError
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)statusCode;
+
+        var errorResponse = new
+        {
+            StatusCode = context.Response.StatusCode,
+            Message = statusCode == HttpStatusCode.InternalServerError ? "An unexpected error occurred." : exception.Message,
+        };
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+    }
+}
