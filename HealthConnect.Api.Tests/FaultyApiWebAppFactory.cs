93ea2b833c9dba2d9dffa39cdb5eb364d68e01a1
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-17 18:16:26 -0300

    feat: created a failed endpoint

---
+++ HealthConnect.Api.Tests/FaultyApiWebAppFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Api.Tests;
+
+public class FaultyApiWebAppFactory : WebApplicationFactory<Program>
+
+{
+    public Action<IApplicationBuilder> ConfigurePipeline { get; set; }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.Configure(app =>
+        {
+            if (ConfigurePipeline != null)
+            {
+                ConfigurePipeline(app);
+            }
+        });
+    }
+
+}
