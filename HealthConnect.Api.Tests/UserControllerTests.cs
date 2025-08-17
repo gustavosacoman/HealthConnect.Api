77abac5fa4eba46311871d2e7a503e14b66eb198
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-17 16:46:28 -0300

    refact: finished to refact UserControllerTest

--- HealthConnect.Api.Tests/UserControllerTests.cs
+++ HealthConnect.Api.Tests/UserControllerTests.cs
@@ -57,4 +57,66 @@ public class UserControllerTests
         Assert.NotNull(user);
         Assert.Equal(email, user.Email);
     }
+
+    [Fact]
+    public async Task CreateUser_ShouldCreateAUser_WhenCalled()
+    {
+
+        var newUser = new UserRegistrationDto
+        {
+            Name = "User test",
+            Email = "userTeste@example.com",
+            Password = "Password123@#",
+            CPF = "58965234525",
+            Phone = "1234567890",
+            BirthDate = new DateOnly(1990, 1, 1),
+        };
+        var response = await _client.PostAsJsonAsync("/api/v1/user", newUser);
+
+        response.EnsureSuccessStatusCode();
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        var user = await response.Content.ReadFromJsonAsync<UserSummaryDto>();
+
+        Assert.NotNull(user);
+        Assert.Equal(newUser.Name, user.Name);
+        Assert.Equal(newUser.Email, user.Email);
+    }
+
+    [Fact]
+    public async Task UpdateUser_ShouldUpdateAUser_WhenCalled()
+    {
+        var userId = "123e4567-e89b-12d3-a456-426614174000";
+
+        var updatedUser = new UserUpdatingDto
+        {
+            Password = "NewPassword123@#",
+            Phone = "2568745689",
+        };
+        var response = await _client.PatchAsJsonAsync($"/api/v1/user/{userId}", updatedUser);
+
+        response.EnsureSuccessStatusCode();
+
+        var user = await response.Content.ReadFromJsonAsync<UserSummaryDto>();
+        Assert.NotNull(user);
+        Assert.Equal(userId, user.Id.ToString());
+        Assert.Equal(updatedUser.Phone, user.Phone);
+        Assert.NotEqual("0987654321", user.Phone);
+    }
+
+    public async Task SoftDeleteUser_ShouldPutADateInDeleleField_WhenCalled()
+    {
+        var userId = "123e4567-e89b-12d3-a456-426614174000";
+        var response = await _client.DeleteAsync($"/api/v1/user/{userId}");
+        
+        response.EnsureSuccessStatusCode();
+
+        var user = await _client.GetAsync($"/api/v1/user/{userId}");
+
+        Assert.Equal(HttpStatusCode.NotFound, user.StatusCode);
+        Assert.Equal("User not found", await user.Content.ReadAsStringAsync());
+    }
+
 }
+        
+        
