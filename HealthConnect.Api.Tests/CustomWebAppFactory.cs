9713d3b7679fc013ed4bd250237db40314807c3e
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-17 00:00:43 -0300

    feat: created a CustomWebAppFactory to create the database in memory e remove the database production

---
+++ HealthConnect.Api.Tests/CustomWebAppFactory.cs
@@ -0,0 +1,34 @@
+﻿using HealthConnect.Api.Tests;
+using HealthConnect.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection; 
+
+namespace HealthConnect.Api;
+
+public class CustomWebAppFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryDbForTesting");
+            });
+
+            var sp = services.BuildServiceProvider();
+            using (var scope = sp.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<AppDbContext>();
+
+                db.Database.EnsureCreated();
+                SeedData.PopulateDatabase(db);
+            }
+        });
+    }
+}
