bae46cc4b753d069ce6c383be611dc0a453bd0cd
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-18 22:34:42 -0300

    feat: testes exception middleware

--- HealthConnect.Api.Tests/FaultyApiWebAppFactory.cs
+++ HealthConnect.Api.Tests/FaultyApiWebAppFactory.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using HealthConnect.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,25 @@ public class FaultyApiWebAppFactory : WebApplicationFactory<Program>
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(
+            d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase($"InMemoryDbForFaultyTest-{Guid.NewGuid()}");
+            });
+
+        });
+
         builder.Configure(app =>
         {
             if (ConfigurePipeline != null)
---
+++ HealthConnect.Api.Tests/GlobalExceptionHandlerMiddlewareTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Api.Tests;
+
+public class GlobalExceptionHandlerMiddlewareTests
+{
+
+    [Fact]
+    public async Task Middleware_WhenKeyNotFoundExceptionIsThrown_ReturnsNotFoundResponse()
+    {
+        var faultyFactory = new FaultyApiWebAppFactory();
+
+        faultyFactory.ConfigurePipeline = app =>
+        {
+            app.UseMiddleware<HealthConnect.Api.Middleweres.GlobalExceptionHandlerMiddleware>();
+            app.Run(context => throw new KeyNotFoundException("test resources not found"));
+        };
+
+        var faultyClient = faultyFactory.CreateClient();
+
+        var response = await faultyClient.GetAsync("/whatever-route-will-fail");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+
+        var content = await response.Content.ReadAsStringAsync();
+        var errorResponse = JsonDocument.Parse(content).RootElement;
+
+        Assert.Equal("application/json", response.Content.Headers.ContentType?.ToString());
+        Assert.Equal(404, errorResponse.GetProperty("StatusCode").GetInt32());
+        Assert.Equal("test resources not found", errorResponse.GetProperty("Message").GetString());
+    }
+}
