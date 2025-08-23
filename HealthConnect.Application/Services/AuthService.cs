228dd1f77525a76c0abd13b41047834a291f7690
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-23 17:00:23 -0300

    feat: created AuthService

---
+++ HealthConnect.Application/Services/AuthService.cs
@@ -0,0 +1,89 @@
+﻿namespace HealthConnect.Application.Services;
+
+using HealthConnect.Application.Dtos.Auth;
+using HealthConnect.Application.Interfaces;
+using HealthConnect.Domain.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+public class AuthService(
+    IUserRepository userRepository,
+    IPasswordHasher passwordHasher,
+    IConfiguration configuration) : IAuthService
+{
+    private readonly IUserRepository _userRepository = userRepository;
+    private readonly IPasswordHasher _passwordHasher = passwordHasher;
+    private readonly IConfiguration _configuration = configuration;
+    public async Task<LoginResponseDto> LoginAsync(LoginRequestDto request)
+    {
+        if (request == null)
+        {
+            throw new NullReferenceException("Login request cannot be null.");
+        }
+
+        if (string.IsNullOrEmpty(request.Email) || string.IsNullOrEmpty(request.Password))
+        {
+            throw new ArgumentException("Email and password must be provided.");
+        }
+
+        var user = await _userRepository.GetUserByEmail(request.Email);
+
+        if (user == null)
+        {
+            throw new UnauthorizedAccessException("Invalid email or password.");
+        }
+
+        var isPasswordValid = _passwordHasher.VerifyPassword(request.Password, user.HashedPassword, user.Salt);
+
+        if (!isPasswordValid)
+        {
+            throw new UnauthorizedAccessException("Invalid email or password.");
+        }
+
+        var token = GenerateToken(user);
+
+        return new LoginResponseDto
+        {
+            Token = token,
+        };
+    }
+
+    private string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var keyString = _configuration["Jwt:Key"]
+            ?? throw new InvalidOperationException("JWT key is not configured.");
+
+        var key = Encoding.ASCII.GetBytes(keyString);
+
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+
+            Expires = DateTime.UtcNow.AddHours(1),
+
+            Issuer = _configuration["Jwt:Issuer"],
+
+            Audience = _configuration["Jwt:Audience"],
+
+            SigningCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(key),
+            SecurityAlgorithms.HmacSha256Signature),
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
