fc269ddfcf76bdecfe97f38001f707aeda02e0c4
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-30 16:11:35 -0300

    feat: created clientRepository

---
+++ HealthConnect.Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,34 @@
+﻿using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+using HealthConnect.Domain.Models;
+using HealthConnect.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthConnect.Infrastructure.Repositories;
+public class ClientRepository(AppDbContext appDbContext) : IClientRepository
+{
+    private readonly AppDbContext _appDbContex = appDbContext;
+
+    public async Task CreateClientAsync(Client client)
+    {
+        await _appDbContex.AddAsync(client);
+    }
+
+    public async Task<IEnumerable<Client>> GetAllClientsAsync()
+    {
+        return await _appDbContex.Clients.Include(c => c.User).ToListAsync();
+    }
+
+    public async Task<Client> GetClientByIdAsync(Guid Id)
+    {
+        return await _appDbContex.Clients
+            .Include(c => c.User)
+            .FirstOrDefaultAsync(c => c.Id == Id);
+    }
+
+    public async Task<Client> GetClientByUserIdAsync(Guid userId)
+    {
+        return await _appDbContex.Clients
+            .Include(c => c.User)
+            .FirstOrDefaultAsync(c => c.UserId == userId);
+    }
+}
