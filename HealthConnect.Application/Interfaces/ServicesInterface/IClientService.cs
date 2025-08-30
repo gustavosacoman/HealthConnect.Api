30f998fa2a0fcfd6d1c9c41261c42801eb63d3c6
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-30 16:38:08 -0300

    feat: created interface for clientService

---
+++ HealthConnect.Application/Interfaces/ServicesInterface/IClientService.cs
@@ -0,0 +1,14 @@
+﻿using HealthConnect.Application.Dtos.Client;
+
+namespace HealthConnect.Application.Interfaces.ServicesInterface;
+
+public interface IClientService
+{
+    public Task<ClientDetailDto> GetClientDetailByIdAsync(Guid userId);
+
+    public Task<ClientSummaryDto> GetClientByIdAsync(Guid clientId);
+
+    public Task<ClientSummaryDto> GetClientByUserIdAsync(Guid userId);
+
+    public Task<IEnumerable<ClientSummaryDto>> GetAllClientsAsync();
+}
