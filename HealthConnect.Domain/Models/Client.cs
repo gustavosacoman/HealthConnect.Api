4e7223afc8e1ae8b6ec7354fe758e37837b75a86
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-30 15:43:07 -0300

    feat: created a Client Entity

---
+++ HealthConnect.Domain/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthConnect.Domain.Models;
+
+public class Client
+{
+    required public Guid Id { get; set; }
+
+    required public Guid UserId { get; set; }
+
+    required public virtual User User { get; set; }
+}
