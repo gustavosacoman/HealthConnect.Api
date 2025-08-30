62ec724fe67165ed9e44bc83641e6ecf40f45dd7
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-30 02:06:06 -0300

    feat: created a place to separate serviceInterfaces and Repositories

---
+++ HealthConnect.Application/Interfaces/RepositoriesInterfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿namespace HealthConnect.Application.Interfaces;
+
+/// <summary>
+/// Represents a unit of work that coordinates the writing of changes and manages transactions.
+/// </summary>
+public interface IUnitOfWork : IAsyncDisposable
+{
+    /// <summary>
+    /// Persists all changes made in the context to the underlying data store asynchronously.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="Task{TResult}"/> representing the asynchronous operation, with the number of state entries written to the data store.
+    /// </returns>
+    Task<int> SaveChangesAsync();
+}
---
+++ HealthConnect.Application/Interfaces/RepositoriesInterfaces/IUserRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+
+using HealthConnect.Domain.Models;
+
+/// <summary>
+/// Provides methods for accessing and managing <see cref="User"/> entities.
+/// </summary>
+public interface IUserRepository
+{
+    /// <summary>
+    /// Retrieves all users.
+    /// </summary>
+    /// <returns>A collection of all <see cref="User"/> entities.</returns>
+    public Task<IEnumerable<User>> GetAllUsersAsync();
+
+    /// <summary>
+    /// Retrieves a user by their unique identifier.
+    /// </summary>
+    /// <param name="Id">The unique identifier of the user.</param>
+    /// <returns>The <see cref="User"/> entity if found; otherwise, <c>null</c>.</returns>
+    public Task<User?> GetUserByIdAsync(Guid Id);
+
+    /// <summary>
+    /// Retrieves a user by their email address.
+    /// </summary>
+    /// <param name="Email">The email address of the user.</param>
+    /// <returns>The <see cref="User"/> entity if found; otherwise, <c>null</c>.</returns>
+    public Task<User?> GetUserByEmailAsync(string Email);
+
+    /// <summary>
+    /// Creates a new user.
+    /// </summary>
+    /// <param name="User">The <see cref="User"/> entity to create.</param>
+    public Task CreateUserAsync(User User);
+
+    public Task<User?> GetDoctorByEmailAsync(string email);
+}
