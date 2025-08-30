21b2c4ce5e40694efa7d29683bdce05901c872a2
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-30 15:46:43 -0300

    feat: added the pattern interfaces to client Entity

--- HealthConnect.Domain/Models/Client.cs
+++ HealthConnect.Domain/Models/Client.cs
@@ -1,10 +1,18 @@
-﻿namespace HealthConnect.Domain.Models;
+﻿using HealthConnect.Domain.Interfaces;
 
-public class Client
+namespace HealthConnect.Domain.Models;
+
+public class Client : IAuditable, ISoftDeletable
 {
     required public Guid Id { get; set; }
 
     required public Guid UserId { get; set; }
 
     required public virtual User User { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
 }
