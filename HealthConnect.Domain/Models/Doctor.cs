dce3ff0b4624d60a3c4391a6ff8dd940fa3ed200
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-08-30 01:59:47 -0300

    feat: created doctor model

---
+++ HealthConnect.Domain/Models/Doctor.cs
@@ -0,0 +1,28 @@
+﻿using HealthConnect.Domain.Interfaces;
+
+namespace HealthConnect.Domain.Models;
+
+public class Doctor: IAuditable, ISoftDeletable
+{
+    public Guid Id { get; set; }
+
+    required public string RQE { get; set; }
+
+    required public string CRM { get; set; }
+
+    public string? Biography { get; set; }
+
+    required public string Specialty { get; set; }
+
+    //public string ProfilePicture { get; set; }
+
+    required public virtual User User { get; set; }
+
+    required public Guid UserId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+}
