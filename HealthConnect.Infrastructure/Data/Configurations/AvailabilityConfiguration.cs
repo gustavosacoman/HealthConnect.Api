1f9b27dcb3622d8282531e8683f961e54dde29ed
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-01 23:43:33 -0300

    feat: set up the database

---
+++ HealthConnect.Infrastructure/Data/Configurations/AvailabilityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using HealthConnect.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HealthConnect.Infrastructure.Data.Configurations;
+
+public class AvailabilityConfiguration : IEntityTypeConfiguration<Availability>
+{
+    public void Configure(EntityTypeBuilder<Availability> builder)
+    {
+        builder.ToTable("Avaiabilities");
+
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.DoctorId)
+            .IsRequired();
+
+        builder.Property(a => a.SlotDateTime)
+               .IsRequired();
+
+        builder.Property(a => a.DurationMinutes)
+            .IsRequired();
+
+        builder.Property(a => a.IsBooked)
+            .IsRequired();
+    }
+}
