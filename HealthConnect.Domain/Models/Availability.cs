f335bc2b343c715006a2b54057737796c615424f
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-01 23:43:19 -0300

    feat: created a Avaibility entity

---
+++ HealthConnect.Domain/Models/Availability.cs
@@ -0,0 +1,23 @@
+﻿namespace HealthConnect.Domain.Models;
+
+using HealthConnect.Domain.Interfaces;
+using System;
+
+public class Availability : IAuditable, ISoftDeletable
+{
+    public Guid Id { get; set; }
+
+    required public Guid DoctorId { get; set; }
+
+    required public DateTime SlotDateTime { get; set; }
+
+    required public int DurationMinutes { get; set; }
+
+    required public bool IsBooked { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+}
