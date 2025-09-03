81394c355d07fd63c2d7948a8c4e2f8e75df34ac
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-02 22:52:15 -0300

    feat: created availabilityRepository

---
+++ HealthConnect.Infrastructure/Repositories/AvailabilityRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace HealthConnect.Infrastructure.Repositories;
+
+using HealthConnect.Application.Dtos.Availability;
+using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+using HealthConnect.Domain.Models;
+using HealthConnect.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class AvailabilityRepository(AppDbContext appDbContext) : IAvailabilityRepository
+{
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+    public async Task CreateAvailabilityAsync(Availability availability)
+    {
+        await _appDbContext.Availabilities.AddAsync(availability);
+    }
+
+    public IQueryable<Availability> GetAllAvailabilityPerDoctor(Guid doctorId)
+    {
+        return _appDbContext.Availabilities
+        .Where(a => a.DoctorId == doctorId);
+    }
+
+    public async Task<Availability> GetAvailabilityByIdAsync(Guid id)
+    {
+        return await _appDbContext.Availabilities.FindAsync(id);
+    }
+}
