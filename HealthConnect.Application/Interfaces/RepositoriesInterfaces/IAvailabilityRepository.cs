55c298c2b1e3af7c40e5851225870b85915f1494
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-02 22:51:48 -0300

    feat: created an interface to availability service and repository

---
+++ HealthConnect.Application/Interfaces/RepositoriesInterfaces/IAvailabilityRepository.cs
@@ -0,0 +1,18 @@
+﻿using HealthConnect.Application.Dtos.Availability;
+using HealthConnect.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+
+public interface IAvailabilityRepository
+{
+    public Task<Availability> GetAvailabilityByIdAsync(Guid id);
+
+    public IQueryable<Availability> GetAllAvailabilityPerDoctor(Guid doctorId);
+
+    public Task CreateAvailabilityAsync(Availability availability);
+}
---
+++ HealthConnect.Application/Interfaces/ServicesInterface/IAvailabilityService.cs
@@ -0,0 +1,15 @@
+﻿namespace HealthConnect.Application.Interfaces.ServicesInterface;
+
+using HealthConnect.Application.Dtos.Availability;
+
+public interface IAvailabilityService
+{
+    public Task<AvailabilitySummaryDto> GetAllAvailabilitiesPerDoctor(Guid doctorId);
+
+    public Task<AvailabilitySummaryDto> CreateAvailability(AvailabilityRegistrationDto availability);
+
+    public Task<AvailabilitySummaryDto> GetAvailabilityById(Guid availabilityId);
+
+    public Task DeleteAvailability(Guid availabilityId);
+
+}
