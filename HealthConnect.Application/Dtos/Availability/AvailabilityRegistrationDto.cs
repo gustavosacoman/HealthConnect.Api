a7c7df781fea9f0916c2d04b3c99c3169305f160
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-02 22:51:23 -0300

    feat: created availabilityDto

---
+++ HealthConnect.Application/Dtos/Availability/AvailabilityRegistrationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthConnect.Application.Dtos.Availability;
+
+public record AvailabilityRegistrationDto
+{
+    public Guid DoctorId { get; init; }
+    public DateTime SlotDateTime { get; init; }
+    public int DurationMinutes { get; init; }
+}
