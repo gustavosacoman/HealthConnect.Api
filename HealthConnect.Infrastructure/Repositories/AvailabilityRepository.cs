fc26e69c15e5d570361ecdc91ff59e15f39bc73c
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-04 01:13:39 -0300

    feat: changed the function to get all availabilities per doctors to have a autoMapper

--- HealthConnect.Infrastructure/Repositories/AvailabilityRepository.cs
+++ HealthConnect.Infrastructure/Repositories/AvailabilityRepository.cs
@@ -5,20 +5,37 @@ using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
 using HealthConnect.Domain.Models;
 using HealthConnect.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using System.Threading.Tasks;
+using AutoMapper.QueryableExtensions;
 
-public class AvailabilityRepository(AppDbContext appDbContext) : IAvailabilityRepository
+public class AvailabilityRepository(
+    AppDbContext appDbContext,
+    IMapper mapper) : IAvailabilityRepository
 {
     private readonly AppDbContext _appDbContext = appDbContext;
+    private readonly IMapper _mapper = mapper;
 
     public async Task CreateAvailabilityAsync(Availability availability)
     {
         await _appDbContext.Availabilities.AddAsync(availability);
     }
 
-    public IQueryable<Availability> GetAllAvailabilityPerDoctor(Guid doctorId)
+    public async Task<bool> HasOverlappingAvailabilityAsync(Guid doctorId, DateTime newSlotStart, DateTime newSlotEnd)
     {
-        return _appDbContext.Availabilities
-        .Where(a => a.DoctorId == doctorId);
+        return await _appDbContext.Availabilities
+            .AnyAsync(a =>
+            a.DoctorId == doctorId &&
+                a.SlotDateTime < newSlotEnd &&
+                (a.SlotDateTime.AddMinutes(a.DurationMinutes) > newSlotStart));
+    }
+
+    public async Task<IEnumerable<TProjection>> GetAllAvailabilityPerDoctor<TProjection>(Guid doctorId)
+    {
+        return await _appDbContext.Availabilities
+            .Where(a => a.DoctorId == doctorId)
+            .ProjectTo<TProjection>(_mapper.ConfigurationProvider)
+            .ToListAsync();
     }
 
     public async Task<Availability> GetAvailabilityByIdAsync(Guid id)
