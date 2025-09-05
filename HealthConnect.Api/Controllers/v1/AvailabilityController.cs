853739d47a2cdf3184eab662e945b617cd99d75d
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-04 22:34:41 -0300

    feat: crated Availability Controller

---
+++ HealthConnect.Api/Controllers/v1/AvailabilityController.cs
@@ -0,0 +1,52 @@
+﻿namespace HealthConnect.Api.Controllers.v1;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Authorization;
+using HealthConnect.Application.Interfaces.ServicesInterface;
+using HealthConnect.Application.Dtos.Availability;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces("application/json")]
+public class AvailabilityController(IAvailabilityService availabilityService) : ControllerBase
+{
+    private readonly IAvailabilityService _availabilityService = availabilityService;
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateAvailability(AvailabilityRegistrationDto availability)
+    {
+        var createdAvailability = await _availabilityService.CreateAvailability(availability);
+
+        return CreatedAtAction(
+            nameof(GetAvailabilityById),
+            new { availabilityId = createdAvailability.Id },
+            createdAvailability);
+    }
+
+    [HttpGet("{availabilityId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAvailabilityById(Guid availabilityId)
+    {
+        var availability = await _availabilityService.GetAvailabilityById(availabilityId);
+        return Ok(availability);
+    }
+
+    [HttpGet("by-doctor/{doctorId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllAvailabilitiesPerDoctor(Guid doctorId)
+    {
+        var availabilities = await _availabilityService.GetAllAvailabilitiesPerDoctor(doctorId);
+        return Ok(availabilities);
+    }
+
+    [HttpDelete]
+    [Route("{availabilityId:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> DeleteAvailability(Guid availabilityId)
+    {
+        await _availabilityService.DeleteAvailability(availabilityId);
+        return NoContent();
+    }
+}
