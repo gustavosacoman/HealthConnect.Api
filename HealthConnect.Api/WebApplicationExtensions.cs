c0200b35f558562c167df794acff52321bce2539
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-07 04:32:33 -0300

    feat: created a db Initializer to createa a two accounts in system to test

---
+++ HealthConnect.Api/WebApplicationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using HealthConnect.Infrastructure; // Para ter acesso ao DbInitializer
+
+public static class WebApplicationExtensions
+{
+    public static async Task SeedDatabaseAsync(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var serviceProvider = scope.ServiceProvider;
+            try
+            {
+                await DbInitializer.InitializeAsync(serviceProvider);
+            }
+            catch (Exception ex)
+            {
+                var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while seeding the database.");
+            }
+        }
+    }
+}
