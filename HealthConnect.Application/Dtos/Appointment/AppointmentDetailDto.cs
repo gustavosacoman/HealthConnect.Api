1712404f62f29064f357fa87965a846144a7a72c
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-07 02:43:55 -0300

    feat: created all Dtos

---
+++ HealthConnect.Application/Dtos/Appointment/AppointmentDetailDto.cs
@@ -0,0 +1,26 @@
+﻿
+namespace HealthConnect.Application.Dtos.Appointment;
+
+public record AppointmentDetailDto
+{
+    public Guid Id { get; init; }
+
+    public Guid DoctorId { get; init; }
+
+    public Guid ClientId { get; init; }
+
+    public Guid AvailabilityId { get; init; }
+
+    public DateTime AppointmentDate { get; init; }
+
+    public int Duration { get; init; }
+
+    public string Status { get; init; }
+
+    public string Notes { get; init; }
+
+    public string DoctorName { get; init; }
+
+    public string ClientName { get; init; }
+
+}
