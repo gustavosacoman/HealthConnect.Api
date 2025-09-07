0e2ecf4ba858c169c5d7531421fffdbddee40c6b
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-07 02:46:39 -0300

    feat: created a interface for appointment service

---
+++ HealthConnect.Application/Interfaces/ServicesInterface/IAppointmentService.cs
@@ -0,0 +1,15 @@
+﻿using HealthConnect.Application.Dtos.Appointment;
+
+namespace HealthConnect.Application.Interfaces.ServicesInterface;
+
+public interface IAppointmentService
+{
+    public Task<AppointmentDetailDto> CreateAppointmentAsync(Guid clientId, AppointmentRegistrationDto appointment);
+
+    public Task<IEnumerable<AppointmentDetailDto>> GetAppointmentsByClientIdAsync(Guid clientId);
+
+    public Task<IEnumerable<AppointmentDetailDto>> GetAppointmentsByDoctorIdAsync(Guid doctorId);
+
+    public Task UpdateAppointmentId(Guid Id, AppointmentUpdatingDto appointment);
+    public Task<AppointmentDetailDto> GetAppointmentByIdAsync(Guid id);
+}
