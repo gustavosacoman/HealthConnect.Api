2689288ba7247ec2636e3d2bb75ccb3a57e59f6e
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-07 22:41:26 -0300

    feat: added migrations

---
+++ HealthConnect.Infrastructure/Migrations/20250908013302_AddAppointments.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthConnect.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAppointments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apointments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ClientId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DoctorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AvailabilityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AppointmentDateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    AppointmentStatus = table.Column<string>(type: "text", nullable: false),
+                    Notes = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Apointments_Avaiabilities_AvailabilityId",
+                        column: x => x.AvailabilityId,
+                        principalTable: "Avaiabilities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Apointments_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Apointments_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_AvailabilityId",
+                table: "Apointments",
+                column: "AvailabilityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_ClientId",
+                table: "Apointments",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Apointments_DoctorId",
+                table: "Apointments",
+                column: "DoctorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Apointments");
+        }
+    }
+}
