621a21c40a32ded7917d092d5586485834a36fd6
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-13 14:42:22 -0300

    feat: created Speciality Entity

---
+++ HealthConnect.Domain/Models/Speciality.cs
@@ -0,0 +1,17 @@
+﻿using HealthConnect.Domain.Interfaces;
+
+namespace HealthConnect.Domain.Models;
+
+public class Speciality : IAuditable, ISoftDeletable
+{
+
+    public Guid Id { get; set; }
+
+    required public string Name { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+}
