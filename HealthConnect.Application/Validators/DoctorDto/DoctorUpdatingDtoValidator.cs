5bd2bc8da22f19db4e687ded70d10011db933717
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-13 16:04:29 -0300

    feat: changed doctor validators to receive SpecialityId

--- HealthConnect.Application/Validators/DoctorDto/DoctorUpdatingDtoValidator.cs
+++ HealthConnect.Application/Validators/DoctorDto/DoctorUpdatingDtoValidator.cs
@@ -9,8 +9,6 @@ public class DoctorUpdatingDtoValidator : AbstractValidator<DoctorUpdatingDto>
     {
         RuleFor(x => x.CRM)
             .MaximumLength(20).WithMessage("CRM must not exceed 20 characters.");
-        RuleFor(x => x.Specialty)
-            .MaximumLength(100).WithMessage("Specialty must not exceed 100 characters.");
         RuleFor(x => x.Biography)
             .MaximumLength(3500).WithMessage("Biography must not exceed 1000 characters.");
         RuleFor(x => x.RQE)
