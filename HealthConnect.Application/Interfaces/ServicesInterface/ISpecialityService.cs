5a8a20d43049487131718979c1bdc74c0d23a669
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-13 14:43:42 -0300

    feat: created SpecialityService and your interface

---
+++ HealthConnect.Application/Interfaces/ServicesInterface/ISpecialityService.cs
@@ -0,0 +1,15 @@
+﻿using HealthConnect.Application.Dtos.Speciality;
+
+namespace HealthConnect.Application.Interfaces.ServicesInterface;
+
+public interface ISpecialityService
+{
+    public Task<SpecialitySummaryDto> GetSpecialityById(Guid id);
+
+    public Task<IEnumerable<SpecialitySummaryDto>> GetAllSpecialities();
+
+    public Task<SpecialitySummaryDto> GetSpecialityByName(string name);
+
+    public Task<SpecialitySummaryDto> CreateSpeciality(SpecialityRegistrationDto specialityDto);
+
+}
---
+++ HealthConnect.Application/Services/SpecialityService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using HealthConnect.Application.Dtos.Speciality;
+using HealthConnect.Application.Interfaces;
+using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+using HealthConnect.Application.Interfaces.ServicesInterface;
+using HealthConnect.Domain.Models;
+using System.Runtime.InteropServices;
+
+namespace HealthConnect.Application.Services;
+
+public class SpecialityService(
+    ISpecialityRepository specialityRepository,
+    IMapper mapper,
+    IUnitOfWork unitOfWork) : ISpecialityService
+{
+    private readonly ISpecialityRepository _specialityRepository = specialityRepository;
+    private readonly IMapper _mapper = mapper;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+    public async Task<SpecialitySummaryDto> CreateSpeciality(SpecialityRegistrationDto specialityDto)
+    {
+        var existingSpeciality = await _specialityRepository.GetSpecialityByNameAsync(specialityDto.Name);
+        if (existingSpeciality != null)
+        {
+            throw new InvalidOperationException("Speciality with the same name already exists.");
+        }
+
+        var speciality = new Speciality
+        {
+            Id = Guid.NewGuid(),
+            Name = specialityDto.Name
+        };
+
+        await _specialityRepository.CreateSpecialityAsync(speciality);
+        await _unitOfWork.SaveChangesAsync();
+
+        return _mapper.Map<SpecialitySummaryDto>(speciality);
+    }
+
+    public async Task<IEnumerable<SpecialitySummaryDto>> GetAllSpecialities()
+    {
+        var specialities = await _specialityRepository.GetAllSpecialitiesAsync();
+        return _mapper.Map<IEnumerable<SpecialitySummaryDto>>(specialities);
+    }
+
+    public async Task<SpecialitySummaryDto> GetSpecialityById(Guid id)
+    {
+        var speciality = await _specialityRepository.GetSpecialityByIdAsync(id);
+        return _mapper.Map<SpecialitySummaryDto>(speciality);
+    }
+
+    public async Task<SpecialitySummaryDto> GetSpecialityByName(string name)
+    {
+        var speciality = await _specialityRepository.GetSpecialityByNameAsync(name);
+        return _mapper.Map<SpecialitySummaryDto>(speciality);
+    }
+}
