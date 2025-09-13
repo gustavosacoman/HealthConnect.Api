50f077445b1810c7c27f98900a014efb4f8e9262
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-13 14:42:58 -0300

    feat: created a repository for speciality

---
+++ HealthConnect.Infrastructure/Repositories/SpecialityRepository.cs
@@ -0,0 +1,30 @@
+﻿using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+using HealthConnect.Domain.Models;
+using HealthConnect.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthConnect.Infrastructure.Repositories;
+
+public class SpecialityRepository(AppDbContext appDbContext) : ISpecialityRepository
+{
+    private readonly AppDbContext _appDbContext1 = appDbContext;
+
+    public async Task<Speciality> GetSpecialityByIdAsync(Guid Id)
+    {
+        return await _appDbContext1.Specialities.FindAsync(Id);
+    }
+
+    public async Task<IEnumerable<Speciality>> GetAllSpecialitiesAsync()
+    {
+        return await _appDbContext1.Specialities.ToListAsync();
+    }
+    public async Task<Speciality> GetSpecialityByNameAsync(string name)
+    {
+        return await _appDbContext1.Specialities.FirstOrDefaultAsync(s => s.Name == name);
+    }
+
+    public async Task CreateSpecialityAsync(Speciality speciality)
+    {
+        await _appDbContext1.Specialities.AddAsync(speciality);
+    }
+}
