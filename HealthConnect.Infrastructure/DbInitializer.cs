e59ac6e8be4736d9c80cdab5507172f2b1e8c5b9
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-14 03:24:50 -0300

    feat: added Speciality to creation of the database

--- HealthConnect.Infrastructure/DbInitializer.cs
+++ HealthConnect.Infrastructure/DbInitializer.cs
@@ -1,10 +1,12 @@
 ﻿using HealthConnect.Application.Dtos.Client;
 using HealthConnect.Application.Dtos.Doctors;
+using HealthConnect.Application.Dtos.Speciality;
+using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
 using HealthConnect.Application.Interfaces.ServicesInterface;
+using HealthConnect.Domain.Models;
 using HealthConnect.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 public static class DbInitializer
 {
@@ -15,9 +17,40 @@ public static class DbInitializer
         await context.Database.EnsureCreatedAsync();
 
         var userService = scope.ServiceProvider.GetRequiredService<IUserService>();
+        var specialityService = scope.ServiceProvider.GetRequiredService<ISpecialityService>();
 
         var doctorEmail = "john@admin.com";
         var clientEmail = "julia@admin.com";
+
+        var names = new List<string>
+        {
+            "Cardiologista",
+            "Dermatologista",
+            "Neurologista",
+            "Pediatra",
+            "Psiquiatra",
+            "Ortopedista",
+            "Ginecologista",
+            "Oftalmologista",
+            "Endocrinologista",
+            "Urologista",
+            "Otorrinolaringologista",
+        };
+
+        foreach (var name in names)
+        {
+            var specialits = await specialityService.GetSpecialityByName(name);
+
+            if (specialits == null)
+            {
+                var newSpecialist = new SpecialityRegistrationDto(name);
+
+                await specialityService.CreateSpeciality(newSpecialist);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        var cardiologySpecialty = await specialityService.GetSpecialityByName("Cardiologista");
 
         if (!await context.Users.AnyAsync(u => u.Email == doctorEmail))
         {
@@ -30,7 +63,7 @@ public static class DbInitializer
                 BirthDate = new DateOnly(1980, 1, 1),
                 RQE = "RQE12345",
                 CRM = "CRM67890",
-                Specialty = "Cardiology",
+                SpecialityId = cardiologySpecialty.Id,
                 Biography = "Experienced cardiologist with over 10 years in practice.",
             };
             await userService.CreateDoctorAsync(doctor);
