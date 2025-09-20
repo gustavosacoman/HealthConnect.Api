0a3484290f24eece65524b36c2a2c0387e97da88
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-20 17:15:33 -0300

    feat: created role Service and your interface

---
+++ HealthConnect.Application/Interfaces/ServicesInterface/IRoleService.cs
@@ -0,0 +1,16 @@
+﻿namespace HealthConnect.Application.Interfaces.ServicesInterface;
+
+using HealthConnect.Application.Dtos.Role;
+
+public interface IRoleService
+{
+    Task<RoleSummaryDto> GetRoleByNameAsync(string roleName);
+
+    Task<IEnumerable<RoleSummaryDto>> GetAllRolesAsync();
+
+    Task<RoleSummaryDto> GetRoleByIdAsync(Guid roleId);
+
+    Task<IEnumerable<RoleSummaryDto>> GetRolesForUserAsync(Guid userId);
+
+    Task CreateRoleAsync(RoleRegistrationDto roleRegistration);
+}
