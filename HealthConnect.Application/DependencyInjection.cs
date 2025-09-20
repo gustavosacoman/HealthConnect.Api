f83862f1a8e2a60297e480c37a344ea8f87515f9
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-20 17:30:24 -0300

    feat: added Role Service  and your interface to DependencyInjection

--- HealthConnect.Application/DependencyInjection.cs
+++ HealthConnect.Application/DependencyInjection.cs
@@ -31,6 +31,7 @@ public static class DependencyInjection
         services.AddScoped<IAvailabilityService, AvailabilityService>();
         services.AddScoped<IClientService, ClientService>();
         services.AddScoped<IAppointmentService, AppointmentService>();
+        services.AddScoped<IRoleService, RoleService>();
         services.AddScoped<ISpecialityService, SpecialityService>();
         services.AddScoped<IDoctorService, DoctorService>();
         return services;
