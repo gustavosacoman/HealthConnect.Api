71442ae1088510bb21b00f722da46749bb4f7058
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-20 17:14:29 -0300

    feat: created RoleRepository and your interface

---
+++ HealthConnect.Application/Interfaces/RepositoriesInterfaces/IRoleRepository.cs
@@ -0,0 +1,18 @@
+﻿using HealthConnect.Domain.Models.Roles;
+
+namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+
+public interface IRoleRepository
+{
+    Task<IEnumerable<Role>> GetRolesForUserAsync(Guid userId);
+
+    Task<IEnumerable<Role>> GetAllRolesAsync();
+
+    Task<Role?> GetRoleByNameAsync(string roleName);
+
+    Task<Role?> GetRoleByIdAsync(Guid roleId);
+
+    Task CreateRoleAsync(Role role);
+
+    Task CreateUserRoleAsync(UserRole userRole);
+}
