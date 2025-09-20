caacda556d8dcc46599dcea785319e2a1775d54e
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-20 17:13:48 -0300

    feat: created role and userRole this is a connection many to many

---
+++ HealthConnect.Domain/Models/Roles/Role.cs
@@ -0,0 +1,18 @@
+﻿using HealthConnect.Domain.Interfaces;
+
+namespace HealthConnect.Domain.Models.Roles;
+public class Role : IAuditable, ISoftDeletable
+{
+    public Guid Id { get; set; }
+
+    required public string Name { get; set; }
+
+    public ICollection<UserRole> UserRoles { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+
+}
