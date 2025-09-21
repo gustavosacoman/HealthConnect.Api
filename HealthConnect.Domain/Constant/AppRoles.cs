387b4ff4546be824434e0a8309ebdbe183b07d0f
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-21 17:12:27 -0300

    feat: Created appRoles to maka pattern in authorize

---
+++ HealthConnect.Domain/Constant/AppRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace HealthConnect.Domain.Constant;
+
+public static class AppRoles
+{
+    public const string Admin = "admin";
+    public const string Doctor = "doctor";
+    public const string Patient = "patient";
+}
