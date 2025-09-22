a77113b88ac108d83a30d7ca8900645382731d8b
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-22 18:40:56 -0300

    feat: created the DoctorCRM entity

---
+++ HealthConnect.Domain/Models/DoctorCRM.cs
@@ -0,0 +1,25 @@
+﻿using HealthConnect.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Domain.Models;
+
+public class DoctorCRM : IAuditable, ISoftDeletable
+{
+    public Guid Id { get; set; }
+
+    public Guid DoctorId { get; set; }
+
+    public string CRMNumber { get; set; }
+
+    public string State { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+}
