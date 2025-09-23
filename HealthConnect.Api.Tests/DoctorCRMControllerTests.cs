10ebeaaf826555ba18e0a744cc527b5a2972ed6a
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-23 17:45:26 -0300

    feat: created DoctoCRMControllerTests to new tests

---
+++ HealthConnect.Api.Tests/DoctorCRMControllerTests.cs
@@ -0,0 +1,79 @@
+﻿using HealthConnect.Application.Dtos.Auth;
+using HealthConnect.Application.Dtos.DoctorCRM;
+using HealthConnect.Infrastructure.Configurations;
+using HealthConnect.Infrastructure.Data;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Api.Tests;
+
+public class DoctorCRMControllerTests : IClassFixture<CustomWebAppFactory>
+{
+    private readonly HttpClient _client;
+    private readonly CustomWebAppFactory _factory;
+    public DoctorCRMControllerTests(CustomWebAppFactory factory)
+    {
+        _factory = factory;
+        _client = _factory.CreateClient();
+
+        using (var scope = _factory.Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<AppDbContext>();
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.
+                Add(new System.Net.Http.Headers
+                .MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            SeedData.PopulateDatabase(db, new CryptoHelper());
+        }
+    }
+
+    private async Task<string> AuthenticateAndGetTokenAsync()
+    {
+        var loginRequest = new LoginRequestDto
+        {
+            Email = "bruno@example.com",
+            Password = "Password123!",
+        };
+        var response = await _client.PostAsJsonAsync("/api/v1/auth/login", loginRequest);
+        response.EnsureSuccessStatusCode();
+
+        var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponseDto>();
+
+        if (loginResponse?.Token == null)
+        {
+            throw new InvalidOperationException("No possible to get the token");
+
+        }
+
+        return loginResponse.Token;
+    }
+
+    [Fact]
+    public async Task GetAllDoctorCRMs_ShouldReturnAllDoctorsInDatabase_WhenCalled()
+    {
+        var token = await AuthenticateAndGetTokenAsync();
+
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var response = await _client.GetAsync("/api/v1/DoctorCRM/all");
+        response.EnsureSuccessStatusCode();
+
+        var crms = await response.Content.ReadFromJsonAsync<IEnumerable<DoctorCRMSummaryDto>>();
+        
+        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        Assert.NotNull(crms);
+        Assert.Equal(3, crms.Count());
+    }
+}
