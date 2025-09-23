d7a2d71ff8baff430e1cc8c699fb612e698f1388
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-22 22:13:25 -0300

    feat: created Doctor CRM Repository and your interface

---
+++ HealthConnect.Application/Interfaces/RepositoriesInterfaces/IDoctorCRMRepository.cs
@@ -0,0 +1,14 @@
+﻿using HealthConnect.Domain.Models;
+
+namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+
+public interface IDoctorCRMRepository
+{
+    Task<DoctorCRM?> GetCRMByCodeAndState(string code, string state);
+
+    IQueryable<DoctorCRM> GetAllAsync();
+
+    Task<DoctorCRM?> GetByIdAsync(Guid id);
+
+    Task CreateCRMAsync(DoctorCRM doctorCRM);
+}
---
+++ HealthConnect.Infrastructure/Repositories/DoctorCRMRepository.cs
@@ -0,0 +1,39 @@
+﻿using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+using HealthConnect.Domain.Models;
+using HealthConnect.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthConnect.Infrastructure.Repositories;
+
+public class DoctorCRMRepository(AppDbContext appDbContext) : IDoctorCRMRepository
+{
+    private readonly AppDbContext _appDbContext = appDbContext;
+
+
+    public async Task CreateCRMAsync(DoctorCRM doctorCRM)
+    {
+        await _appDbContext.DoctorCRMs.AddAsync(doctorCRM);
+    }
+
+    public async Task<DoctorCRM?> GetByIdAsync(Guid id)
+    {
+        return await _appDbContext.DoctorCRMs.FindAsync(id);
+    }
+
+    public IQueryable<DoctorCRM> GetAllAsync()
+    {
+        return _appDbContext.DoctorCRMs.AsNoTracking();
+    }
+
+    public async Task<DoctorCRM?> GetCRMByCodeAndState(string code, string state)
+    {
+        return await _appDbContext
+            .DoctorCRMs
+            .FirstOrDefaultAsync(c => c.CRMNumber == code && c.State == state);
+    }
+}
