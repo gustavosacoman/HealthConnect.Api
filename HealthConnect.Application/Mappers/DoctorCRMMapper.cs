a8da8e2ade99b548ee956fb39e48d6771281069e
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-22 23:03:02 -0300

    feat: created crm mapper

---
+++ HealthConnect.Application/Mappers/DoctorCRMMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HealthConnect.Application.Dtos.DoctorCRM;
+using HealthConnect.Domain.Models;
+
+namespace HealthConnect.Application.Mappers;
+
+public class DoctorCRMMapper : Profile
+{
+    public DoctorCRMMapper()
+    {
+        CreateMap<DoctorCRM, DoctorCRMSummaryDto>()
+            .ForMember(dest => dest.DoctorName, opt => opt.MapFrom(src => src.Doctor.User.Name));
+    }
+}
