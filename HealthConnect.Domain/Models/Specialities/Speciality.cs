e3dfd505b30549861088c38eeb49c84c1d01005e
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-25 23:14:40 -0300

    feat: changed the Speciality entity to a new folder and created DoctorSpeciality

---
+++ HealthConnect.Domain/Models/Specialities/Speciality.cs
@@ -0,0 +1,21 @@
+﻿using HealthConnect.Domain.Interfaces;
+
+namespace HealthConnect.Domain.Models.Specialities;
+
+public class Speciality : IAuditable, ISoftDeletable
+{
+
+    public Guid Id { get; set; }
+
+    required public string Name { get; set; }
+
+    public ICollection<DoctorSpeciality> DoctorSpecialities { get; set; }
+
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public DateTime? DeletedAt { get; set; }
+}
