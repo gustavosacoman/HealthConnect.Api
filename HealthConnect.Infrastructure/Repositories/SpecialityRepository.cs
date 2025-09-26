6dcbc3fad9be0f85de5ae4b2486b45b8e1df4131
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-25 23:21:26 -0300

    feat: changed the SpecialityRepository to add and return DoctorSpecialities

--- HealthConnect.Infrastructure/Repositories/SpecialityRepository.cs
+++ HealthConnect.Infrastructure/Repositories/SpecialityRepository.cs
@@ -6,24 +6,38 @@ using HealthConnect.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 public class SpecialityRepository(AppDbContext appDbContext) : ISpecialityRepository
 {
-    private readonly AppDbContext _appDbContext1 = appDbContext;
+    private readonly AppDbContext _appDbContext = appDbContext;
 
     public async Task<Speciality> GetSpecialityByIdAsync(Guid Id)
     {
-        return await _appDbContext1.Specialities.FindAsync(Id);
+        return await _appDbContext.Specialities.FindAsync(Id);
     }
 
     public async Task<IEnumerable<Speciality>> GetAllSpecialitiesAsync()
     {
-        return await _appDbContext1.Specialities.ToListAsync();
+        return await _appDbContext.Specialities.ToListAsync();
     }
+
     public async Task<Speciality> GetSpecialityByNameAsync(string name)
     {
-        return await _appDbContext1.Specialities.FirstOrDefaultAsync(s => s.Name == name);
+        return await _appDbContext.Specialities.FirstOrDefaultAsync(s => s.Name == name);
+    }
+
+    public async Task<IEnumerable<Speciality>> GetSpecialitiesForDoctor(Guid doctorId)
+    {
+        return await _appDbContext.Specialities
+            .Where(s => s.DoctorSpecialities
+            .Any(ds => ds.DoctorId == doctorId))
+            .ToListAsync();
+    }
+
+    public async Task CreateDoctorSpeciality(DoctorSpeciality doctorSpeciality)
+    {
+        await _appDbContext.DoctorSpecialities.AddAsync(doctorSpeciality);
     }
 
     public async Task CreateSpecialityAsync(Speciality speciality)
     {
-        await _appDbContext1.Specialities.AddAsync(speciality);
+        await _appDbContext.Specialities.AddAsync(speciality);
     }
 }
