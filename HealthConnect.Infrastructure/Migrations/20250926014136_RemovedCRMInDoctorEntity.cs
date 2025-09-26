256acd00d818af137ba9559ec0d80a84e457ebae
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-25 22:42:12 -0300

    fix: created new migration to remove the CMR in doctor

---
+++ HealthConnect.Infrastructure/Migrations/20250926014136_RemovedCRMInDoctorEntity.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthConnect.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedCRMInDoctorEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Doctors_CRM",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "CRM",
+                table: "Doctors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CRMNumber",
+                table: "DoctorCRMs",
+                type: "character varying(6)",
+                maxLength: 6,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CRM",
+                table: "Doctors",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CRMNumber",
+                table: "DoctorCRMs",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(6)",
+                oldMaxLength: 6);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_CRM",
+                table: "Doctors",
+                column: "CRM",
+                unique: true);
+        }
+    }
+}
