0f7a0120308370cc6f48e460d636bfb5cdf0bb5a
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-27 14:12:17 -0300

    cheore: documented and fixed functions order

--- HealthConnect.Infrastructure/Data/AppDbContext.cs
+++ HealthConnect.Infrastructure/Data/AppDbContext.cs
@@ -25,26 +25,53 @@ public class AppDbContext : DbContext
     /// </summary>
     public DbSet<User> Users { get; set; }
 
+    /// <summary>
+    /// Gets or sets the doctors in the database.
+    /// </summary>
     public DbSet<Doctor> Doctors { get; set; }
 
+    /// <summary>
+    /// Gets or sets the clients in the database.
+    /// </summary>
     public DbSet<Client> Clients { get; set; }
 
+    /// <summary>
+    /// Gets or sets the availabilities in the database.
+    /// </summary>
     public DbSet<Availability> Availabilities { get; set; }
 
+    /// <summary>
+    /// Gets or sets the appointments in the database.
+    /// </summary>
     public DbSet<Appointment> Appointments { get; set; }
 
+    /// <summary>
+    /// Gets or sets the specialities in the database.
+    /// </summary>
     public DbSet<Speciality> Specialities { get; set; }
 
+    /// <summary>
+    /// Gets or sets the roles in the database.
+    /// </summary>
     public DbSet<Role> Roles { get; set; }
 
+    /// <summary>
+    /// Gets or sets the user roles in the database.
+    /// </summary>
     public DbSet<UserRole> UserRoles { get; set; }
 
+    /// <summary>
+    /// Gets or sets the doctor CRMs in the database.
+    /// </summary>
     public DbSet<DoctorCRM> DoctorCRMs { get; set; }
 
+    /// <summary>
+    /// Gets or sets the doctor specialities in the database.
+    /// </summary>
     public DbSet<DoctorSpeciality> DoctorSpecialities { get; set; }
 
     /// <summary>
-    /// Saves all changes made in this context to the database asynchronously.
+    /// Saves all changes made in this context to the database asynchronously and calls the updateAuditableFields.
     /// </summary>
     /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
     /// <returns>The number of state entries written to the database.</returns>
@@ -52,6 +79,16 @@ public class AppDbContext : DbContext
     {
         UpdateAuditableFields();
         return base.SaveChangesAsync(cancellationToken);
+    }
+
+    /// <summary>
+    /// Configures the model for the context.
+    /// </summary>
+    /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 
     /// <summary>
@@ -76,15 +113,5 @@ public class AppDbContext : DbContext
                 entry.Property(p => p.UpdatedAt).CurrentValue = timestamp;
             }
         }
-    }
-
-    /// <summary>
-    /// Configures the model for the context.
-    /// </summary>
-    /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 }
