6a5cdb9be3757d58b5a8789d954b9c73eb03d3d2
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-27 13:47:29 -0300

    chore: documented the code and fixed identantion in start attributes and change the order of the namespace

--- HealthConnect.Application/Interfaces/RepositoriesInterfaces/IDoctorRepository.cs
+++ HealthConnect.Application/Interfaces/RepositoriesInterfaces/IDoctorRepository.cs
@@ -1,26 +1,73 @@
-﻿using HealthConnect.Domain.Models;
+﻿namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+
+using HealthConnect.Domain.Models;
 using HealthConnect.Domain.Models.Specialities;
 
-namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
-
+/// <summary>
+/// Repository interface for managing <see cref="Doctor"/> entities and their relationships with specialities.
+/// </summary>
 public interface IDoctorRepository
 {
-    public IQueryable<Doctor> GetAllDoctors();
+    /// <summary>
+    /// Gets all doctors.
+    /// </summary>
+    /// <returns>An <see cref="IQueryable{Doctor}"/> of all doctors.</returns>
+    IQueryable<Doctor> GetAllDoctors();
 
-    public Task<Doctor?> GetDoctorById(Guid id);
+    /// <summary>
+    /// Gets a doctor by their unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the doctor.</param>
+    /// <returns>A <see cref="Task{Doctor}"/> representing the asynchronous operation. The result contains the doctor if found; otherwise, null.</returns>
+    Task<Doctor?> GetDoctorById(Guid id);
 
-    public Task<Doctor?> GetDoctorByRQE(string rqe);
+    /// <summary>
+    /// Gets a doctor by their RQE (Registro de Qualificação de Especialista).
+    /// </summary>
+    /// <param name="rqe">The RQE of the doctor.</param>
+    /// <returns>A <see cref="Task{Doctor}"/> representing the asynchronous operation. The result contains the doctor if found; otherwise, null.</returns>
+    Task<Doctor?> GetDoctorByRQE(string rqe);
 
-    public Task CreateDoctor(Doctor doctor);
+    /// <summary>
+    /// Creates a new doctor.
+    /// </summary>
+    /// <param name="doctor">The doctor entity to create.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task CreateDoctor(Doctor doctor);
 
-    public IQueryable<Doctor> GetAllDoctorsBySpecialityAsync(Guid specialityId);
+    /// <summary>
+    /// Gets all doctors by speciality.
+    /// </summary>
+    /// <param name="specialityId">The unique identifier of the speciality.</param>
+    /// <returns>An <see cref="IQueryable{Doctor}"/> of doctors with the specified speciality.</returns>
+    IQueryable<Doctor> GetAllDoctorsBySpecialityAsync(Guid specialityId);
 
-    public Task AddDoctorLinkToSpeciality(DoctorSpeciality doctorSpeciality);
+    /// <summary>
+    /// Adds a link between a doctor and a speciality.
+    /// </summary>
+    /// <param name="doctorSpeciality">The doctor-speciality link to add.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    Task AddDoctorLinkToSpeciality(DoctorSpeciality doctorSpeciality);
 
-    public Task RemoveDoctorLinkToSpeciality(DoctorSpeciality doctorSpeciality);
+    /// <summary>
+    /// Removes a link between a doctor and a speciality.
+    /// </summary>
+    /// <param name="doctorSpeciality">The doctor-speciality link to remove.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    void RemoveDoctorLinkToSpeciality(DoctorSpeciality doctorSpeciality);
 
-    public Task<DoctorSpeciality> GetDoctorSpecialityLink(Guid doctorId, Guid specialityId);
+    /// <summary>
+    /// Gets the link between a doctor and a speciality.
+    /// </summary>
+    /// <param name="doctorId">The unique identifier of the doctor.</param>
+    /// <param name="specialityId">The unique identifier of the speciality.</param>
+    /// <returns>A <see cref="Task{DoctorSpeciality}"/> representing the asynchronous operation. The result contains the link if found.</returns>
+    Task<DoctorSpeciality?> GetDoctorSpecialityLink(Guid doctorId, Guid specialityId);
 
-    public Task<Doctor?> GetDoctorByUserId(Guid userId);
-
+    /// <summary>
+    /// Gets a doctor by their user identifier.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user.</param>
+    /// <returns>A <see cref="Task{Doctor}"/> representing the asynchronous operation. The result contains the doctor if found; otherwise, null.</returns>
+    Task<Doctor?> GetDoctorByUserId(Guid userId);
 }
