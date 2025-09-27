7a0202d0ef813399cbd08e04a2ec8ae8501c9228
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-27 13:42:48 -0300

    chore: documented and now namespace comes first

--- HealthConnect.Application/Interfaces/RepositoriesInterfaces/IRoleRepository.cs
+++ HealthConnect.Application/Interfaces/RepositoriesInterfaces/IRoleRepository.cs
@@ -1,18 +1,50 @@
-﻿using HealthConnect.Domain.Models.Roles;
+﻿namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
 
-namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+using HealthConnect.Domain.Models.Roles;
 
+/// <summary>
+/// Provides methods for managing roles and user-role associations.
+/// </summary>
 public interface IRoleRepository
 {
+    /// <summary>
+    /// Gets the roles assigned to a specific user.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user.</param>
+    /// <returns>A collection of roles for the user.</returns>
     Task<IEnumerable<Role>> GetRolesForUserAsync(Guid userId);
 
+    /// <summary>
+    /// Gets all roles in the system.
+    /// </summary>
+    /// <returns>A collection of all roles.</returns>
     Task<IEnumerable<Role>> GetAllRolesAsync();
 
+    /// <summary>
+    /// Gets a role by its name.
+    /// </summary>
+    /// <param name="roleName">The name of the role.</param>
+    /// <returns>The role if found; otherwise, null.</returns>
     Task<Role?> GetRoleByNameAsync(string roleName);
 
+    /// <summary>
+    /// Gets a role by its unique identifier.
+    /// </summary>
+    /// <param name="roleId">The unique identifier of the role.</param>
+    /// <returns>The role if found; otherwise, null.</returns>
     Task<Role?> GetRoleByIdAsync(Guid roleId);
 
+    /// <summary>
+    /// Creates a new role.
+    /// </summary>
+    /// <param name="role">The role to create.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     Task CreateRoleAsync(Role role);
 
+    /// <summary>
+    /// Associates a user with a role.
+    /// </summary>
+    /// <param name="userRole">The user-role association to create.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     Task CreateUserRoleAsync(UserRole userRole);
 }
