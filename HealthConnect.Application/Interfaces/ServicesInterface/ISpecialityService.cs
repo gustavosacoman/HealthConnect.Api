0925e27e66b48ae67b700e8c14a74ce1b9bb3dfb
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-27 14:59:12 -0300

    chore: documented Speciality

--- HealthConnect.Application/Interfaces/ServicesInterface/ISpecialityService.cs
+++ HealthConnect.Application/Interfaces/ServicesInterface/ISpecialityService.cs
@@ -1,15 +1,36 @@
-﻿using HealthConnect.Application.Dtos.Speciality;
+﻿namespace HealthConnect.Application.Interfaces.ServicesInterface;
 
-namespace HealthConnect.Application.Interfaces.ServicesInterface;
+using HealthConnect.Application.Dtos.Speciality;
 
+/// <summary>
+/// Provides operations for managing medical specialities.
+/// </summary>
 public interface ISpecialityService
 {
-    public Task<SpecialitySummaryDto> GetSpecialityById(Guid id);
+    /// <summary>
+    /// Gets a speciality by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the speciality.</param>
+    /// <returns>A <see cref="SpecialitySummaryDto"/> representing the speciality.</returns>
+    Task<SpecialitySummaryDto> GetSpecialityById(Guid id);
 
-    public Task<IEnumerable<SpecialitySummaryDto>> GetAllSpecialities();
+    /// <summary>
+    /// Gets all available specialities.
+    /// </summary>
+    /// <returns>An enumerable collection of <see cref="SpecialitySummaryDto"/>.</returns>
+    Task<IEnumerable<SpecialitySummaryDto>> GetAllSpecialities();
 
-    public Task<SpecialitySummaryDto> GetSpecialityByName(string name);
+    /// <summary>
+    /// Gets a speciality by its name.
+    /// </summary>
+    /// <param name="name">The name of the speciality.</param>
+    /// <returns>A <see cref="SpecialitySummaryDto"/> representing the speciality.</returns>
+    Task<SpecialitySummaryDto> GetSpecialityByName(string name);
 
-    public Task<SpecialitySummaryDto> CreateSpeciality(SpecialityRegistrationDto specialityDto);
-
+    /// <summary>
+    /// Creates a new speciality.
+    /// </summary>
+    /// <param name="specialityDto">The registration details for the new speciality.</param>
+    /// <returns>A <see cref="SpecialitySummaryDto"/> representing the created speciality.</returns>
+    Task<SpecialitySummaryDto> CreateSpeciality(SpecialityRegistrationDto specialityDto);
 }
--- HealthConnect.Application/Services/SpecialityService.cs
+++ HealthConnect.Application/Services/SpecialityService.cs
@@ -1,23 +1,26 @@
-﻿using AutoMapper;
+﻿namespace HealthConnect.Application.Services;
+
+using AutoMapper;
 using HealthConnect.Application.Dtos.Speciality;
 using HealthConnect.Application.Interfaces;
 using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
 using HealthConnect.Application.Interfaces.ServicesInterface;
-using HealthConnect.Domain.Models;
 using HealthConnect.Domain.Models.Specialities;
-using System.Runtime.InteropServices;
 
-namespace HealthConnect.Application.Services;
-
+/// <summary>
+/// Provides handling of speciality business rules for retrieval, creation, update, and deletion.
+/// </summary>
 public class SpecialityService(
     ISpecialityRepository specialityRepository,
     IMapper mapper,
-    IUnitOfWork unitOfWork) : ISpecialityService
+    IUnitOfWork unitOfWork)
+    : ISpecialityService
 {
     private readonly ISpecialityRepository _specialityRepository = specialityRepository;
     private readonly IMapper _mapper = mapper;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
+    /// <inheritdoc/>
     public async Task<SpecialitySummaryDto> CreateSpeciality(SpecialityRegistrationDto specialityDto)
     {
         var existingSpeciality = await _specialityRepository.GetSpecialityByNameAsync(specialityDto.Name);
@@ -29,7 +32,7 @@ public class SpecialityService(
         var speciality = new Speciality
         {
             Id = Guid.NewGuid(),
-            Name = specialityDto.Name
+            Name = specialityDto.Name,
         };
 
         await _specialityRepository.CreateSpecialityAsync(speciality);
@@ -38,18 +41,21 @@ public class SpecialityService(
         return _mapper.Map<SpecialitySummaryDto>(speciality);
     }
 
+    /// <inheritdoc/>
     public async Task<IEnumerable<SpecialitySummaryDto>> GetAllSpecialities()
     {
         var specialities = await _specialityRepository.GetAllSpecialitiesAsync();
         return _mapper.Map<IEnumerable<SpecialitySummaryDto>>(specialities);
     }
 
+    /// <inheritdoc/>
     public async Task<SpecialitySummaryDto> GetSpecialityById(Guid id)
     {
         var speciality = await _specialityRepository.GetSpecialityByIdAsync(id);
         return _mapper.Map<SpecialitySummaryDto>(speciality);
     }
 
+    /// <inheritdoc/>
     public async Task<SpecialitySummaryDto> GetSpecialityByName(string name)
     {
         var speciality = await _specialityRepository.GetSpecialityByNameAsync(name);
