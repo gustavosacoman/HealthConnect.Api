94e8c315738314e463763fc9c0b3632ea1ce66b3
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-27 14:51:05 -0300

    chore: documented constants

--- HealthConnect.Domain/Constant/AppRoles.cs
+++ HealthConnect.Domain/Constant/AppRoles.cs
@@ -1,8 +1,22 @@
 ﻿namespace HealthConnect.Domain.Constant;
 
+/// <summary>
+/// Provides application role constants.
+/// </summary>
 public static class AppRoles
 {
+    /// <summary>
+    /// Represents the administrator role.
+    /// </summary>
     public const string Admin = "admin";
+
+    /// <summary>
+    /// Represents the doctor role.
+    /// </summary>
     public const string Doctor = "doctor";
+
+    /// <summary>
+    /// Represents the patient role.
+    /// </summary>
     public const string Patient = "patient";
 }
