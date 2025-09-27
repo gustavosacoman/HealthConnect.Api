4506cd8bb66089b8e0a40cc4429229ad7b600f6c
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-27 15:03:57 -0300

    chore: added documentation in service

--- HealthConnect.Application/Interfaces/ServicesInterface/IDoctorService.cs
+++ HealthConnect.Application/Interfaces/ServicesInterface/IDoctorService.cs
@@ -1,20 +1,58 @@
-﻿using HealthConnect.Application.Dtos.Doctors;
+﻿namespace HealthConnect.Application.Interfaces.ServicesInterface;
 
-namespace HealthConnect.Application.Interfaces.ServicesInterface;
+using HealthConnect.Application.Dtos.Doctors;
 
+/// <summary>
+/// Provides methods for managing and retrieving doctor information.
+/// </summary>
 public interface IDoctorService
 {
-    public Task<IEnumerable<DoctorSummaryDto>> GetAllDoctorsAsync();
+    /// <summary>
+    /// Retrieves all doctors with summary information.
+    /// </summary>
+    /// <returns>A collection of <see cref="DoctorSummaryDto"/>.</returns>
+    Task<IEnumerable<DoctorSummaryDto>> GetAllDoctorsAsync();
 
-    public Task<DoctorDetailDto> GetDoctorByIdDetailAsync(Guid id);
+    /// <summary>
+    /// Retrieves detailed information for a doctor by their unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the doctor.</param>
+    /// <returns>A <see cref="DoctorDetailDto"/> containing detailed doctor information.</returns>
+    Task<DoctorDetailDto> GetDoctorByIdDetailAsync(Guid id);
 
-    public Task<DoctorSummaryDto> GetDoctorByRQEAsync(string rqe);
+    /// <summary>
+    /// Retrieves summary information for a doctor by their RQE (Registro de Qualificação de Especialista).
+    /// </summary>
+    /// <param name="rqe">The RQE of the doctor.</param>
+    /// <returns>A <see cref="DoctorSummaryDto"/> containing summary doctor information.</returns>
+    Task<DoctorSummaryDto> GetDoctorByRQEAsync(string rqe);
 
-    public Task<DoctorSummaryDto> GetDoctorByIdAsync(Guid id);
+    /// <summary>
+    /// Retrieves summary information for a doctor by their unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the doctor.</param>
+    /// <returns>A <see cref="DoctorSummaryDto"/> containing summary doctor information.</returns>
+    Task<DoctorSummaryDto> GetDoctorByIdAsync(Guid id);
 
-    public Task<DoctorSummaryDto> UpdateDoctorAsync(Guid id, DoctorUpdatingDto doctorUpdatingDto);
+    /// <summary>
+    /// Updates the information of a doctor.
+    /// </summary>
+    /// <param name="id">The unique identifier of the doctor.</param>
+    /// <param name="doctorUpdatingDto">The updated doctor information.</param>
+    /// <returns>A <see cref="DoctorSummaryDto"/> containing the updated summary information.</returns>
+    Task<DoctorSummaryDto> UpdateDoctorAsync(Guid id, DoctorUpdatingDto doctorUpdatingDto);
 
-    public Task<IEnumerable<DoctorDetailDto>> GetAllDoctorsBySpecialityAsync(Guid specialityId);
+    /// <summary>
+    /// Retrieves all doctors with detailed information by their speciality.
+    /// </summary>
+    /// <param name="specialityId">The unique identifier of the speciality.</param>
+    /// <returns>A collection of <see cref="DoctorDetailDto"/>.</returns>
+    Task<IEnumerable<DoctorDetailDto>> GetAllDoctorsBySpecialityAsync(Guid specialityId);
 
-    public Task<DoctorDetailDto> GetDoctoDetailByUserIdAsync(Guid userId);
+    /// <summary>
+    /// Retrieves detailed information for a doctor by their user identifier.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user.</param>
+    /// <returns>A <see cref="DoctorDetailDto"/> containing detailed doctor information.</returns>
+    Task<DoctorDetailDto> GetDoctoDetailByUserIdAsync(Guid userId);
 }
