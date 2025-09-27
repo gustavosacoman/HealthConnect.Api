2e780bbc32f1afd8eb880e0622ef81774fed869f
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-26 22:55:34 -0300

    feat: made a new migrations

---
+++ HealthConnect.Infrastructure/Migrations/20250927014657_AddedDoctorSpeciality.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthConnect.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDoctorSpeciality : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Specialities_SpecialityId",
+                table: "Doctors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Doctors_SpecialityId",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "SpecialityId",
+                table: "Doctors");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Specialities",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DoctorSpecialities",
+                columns: table => new
+                {
+                    DoctorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    SpecialityId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RqeNumber = table.Column<string>(type: "character varying(9)", maxLength: 9, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DoctorSpecialities", x => new { x.DoctorId, x.SpecialityId });
+                    table.ForeignKey(
+                        name: "FK_DoctorSpecialities_Doctors_DoctorId",
+                        column: x => x.DoctorId,
+                        principalTable: "Doctors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DoctorSpecialities_Specialities_SpecialityId",
+                        column: x => x.SpecialityId,
+                        principalTable: "Specialities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoctorSpecialities_RqeNumber",
+                table: "DoctorSpecialities",
+                column: "RqeNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DoctorSpecialities_SpecialityId",
+                table: "DoctorSpecialities",
+                column: "SpecialityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DoctorSpecialities");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Specialities");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SpecialityId",
+                table: "Doctors",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_SpecialityId",
+                table: "Doctors",
+                column: "SpecialityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Specialities_SpecialityId",
+                table: "Doctors",
+                column: "SpecialityId",
+                principalTable: "Specialities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
