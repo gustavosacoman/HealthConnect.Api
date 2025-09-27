4feb5ee093e00992545ca0f6b6d19566515a532a
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-27 13:52:58 -0300

    chore: documented all code and change the namespace order

--- HealthConnect.Application/Interfaces/RepositoriesInterfaces/IClientRepository.cs
+++ HealthConnect.Application/Interfaces/RepositoriesInterfaces/IClientRepository.cs
@@ -1,14 +1,36 @@
-﻿using HealthConnect.Domain.Models;
+﻿namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
 
-namespace HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+using HealthConnect.Domain.Models;
 
+/// <summary>
+/// Provides methods for accessing and managing <see cref="Client"/> entities in the data store.
+/// </summary>
 public interface IClientRepository
 {
-    public Task<Client> GetClientByIdAsync(Guid Id);
+    /// <summary>
+    /// Retrieves a <see cref="Client"/> by its unique identifier.
+    /// </summary>
+    /// <param name="Id">The unique identifier of the client.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the client if found; otherwise, null.</returns>
+    public Task<Client?> GetClientByIdAsync(Guid id);
 
-    public Task<Client> GetClientByUserIdAsync(Guid userId);
+    /// <summary>
+    /// Retrieves a <see cref="Client"/> by the associated user identifier.
+    /// </summary>
+    /// <param name="userId">The unique identifier of the user.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the client if found; otherwise, null.</returns>
+    public Task<Client?> GetClientByUserIdAsync(Guid userId);
 
+    /// <summary>
+    /// Gets a queryable collection of all <see cref="Client"/> entities.
+    /// </summary>
+    /// <returns>An <see cref="IQueryable{Client}"/> representing all clients.</returns>
     public IQueryable<Client> GetAllClientsAsync();
 
+    /// <summary>
+    /// Creates a new <see cref="Client"/> in the data store.
+    /// </summary>
+    /// <param name="client">The client entity to create.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public Task CreateClientAsync(Client client);
 }
--- HealthConnect.Infrastructure/Repositories/ClientRepository.cs
+++ HealthConnect.Infrastructure/Repositories/ClientRepository.cs
@@ -1,24 +1,31 @@
-﻿using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+﻿namespace HealthConnect.Infrastructure.Repositories;
+
+using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
 using HealthConnect.Domain.Models;
 using HealthConnect.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-namespace HealthConnect.Infrastructure.Repositories;
+/// <summary>
+/// Repository for managing patients.
+/// </summary>
 public class ClientRepository(AppDbContext appDbContext) : IClientRepository
 {
     private readonly AppDbContext _appDbContex = appDbContext;
 
+    /// <inheritdoc/>
     public async Task CreateClientAsync(Client client)
     {
         await _appDbContex.Clients.AddAsync(client);
     }
 
+    /// <inheritdoc/>
     public IQueryable<Client> GetAllClientsAsync()
     {
         return _appDbContex.Clients.AsNoTracking();
     }
 
-    public async Task<Client> GetClientByIdAsync(Guid Id)
+    /// <inheritdoc/>
+    public async Task<Client?> GetClientByIdAsync(Guid Id)
     {
         return await _appDbContex.Clients
             .Include(c => c.User)
@@ -27,7 +34,8 @@ public class ClientRepository(AppDbContext appDbContext) : IClientRepository
             .FirstOrDefaultAsync(c => c.Id == Id);
     }
 
-    public async Task<Client> GetClientByUserIdAsync(Guid userId)
+    /// <inheritdoc/>
+    public async Task<Client?> GetClientByUserIdAsync(Guid userId)
     {
         return await _appDbContex.Clients
             .Include(c => c.User)
