29f744e2b24fc31c7fff2e8296a845b0d6f52e32
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-28 14:50:43 -0300

    feat: added mapper

---
+++ HealthConnect.Application/Mappers/DoctorOfficeMapper.cs
@@ -0,0 +1,20 @@
+﻿namespace HealthConnect.Application.Mappers;
+
+using AutoMapper;
+using HealthConnect.Application.Dtos.DoctorOffice;
+using HealthConnect.Domain.Models;
+
+/// <summary>
+/// AutoMapper profile for mapping <see cref="DoctorOffice"/> to <see cref="DoctorOfficeSummaryDto"/>.
+/// </summary>
+public class DoctorOfficeMapper : Profile
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DoctorOfficeMapper"/> class.
+    /// Configures mapping from <see cref="DoctorOffice"/> to <see cref="DoctorOfficeSummaryDto"/>.
+    /// </summary>
+    public DoctorOfficeMapper()
+    {
+        CreateMap<DoctorOffice, DoctorOfficeSummaryDto>();
+    }
+}
