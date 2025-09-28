a55f5c037ecea86025e4566d237c22b975846e78
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-28 14:50:36 -0300

    feat: added mapper to doctor office and your interface

---
+++ HealthConnect.Application/Interfaces/ServicesInterface/IDoctorOfficeService.cs
@@ -0,0 +1,51 @@
+﻿namespace HealthConnect.Application.Interfaces.ServicesInterface;
+
+using HealthConnect.Application.Dtos.DoctorOffice;
+
+/// <summary>
+/// Provides operations for managing doctor offices.
+/// </summary>
+public interface IDoctorOfficeService
+{
+    /// <summary>
+    /// Gets the summary information for a doctor office by its unique identifier.
+    /// </summary>
+    /// <param name="id">The unique identifier of the doctor office.</param>
+    /// <returns>
+    /// A <see cref="DoctorOfficeSummaryDto"/> representing the office, or <c>null</c> if not found.
+    /// </returns>
+    Task<DoctorOfficeSummaryDto?> GetOfficeByIdAsync(Guid id);
+
+    /// <summary>
+    /// Gets summary information for all doctor offices.
+    /// </summary>
+    /// <returns>
+    /// An enumerable collection of <see cref="DoctorOfficeSummaryDto"/>.
+    /// </returns>
+    Task<IEnumerable<DoctorOfficeSummaryDto>> GetAllDoctorOfficeAsync();
+
+    /// <summary>
+    /// Gets summary information for all offices associated with a specific doctor.
+    /// </summary>
+    /// <param name="doctorId">The unique identifier of the doctor.</param>
+    /// <returns>
+    /// An enumerable collection of <see cref="DoctorOfficeSummaryDto"/>.
+    /// </returns>
+    Task<IEnumerable<DoctorOfficeSummaryDto>> GetOfficeByDoctorIdAsync(Guid doctorId);
+
+    /// <summary>
+    /// Gets the primary office for a specific doctor.
+    /// </summary>
+    /// <param name="doctorId">The unique identifier of the doctor.</param>
+    /// <returns>
+    /// A <see cref="DoctorOfficeSummaryDto"/> representing the primary office, or <c>null</c> if not found.
+    /// </returns>
+    Task<DoctorOfficeSummaryDto?> GetPrimaryOfficeByDoctorIdAsync(Guid doctorId);
+
+    /// <summary>
+    /// Creates a new doctor office.
+    /// </summary>
+    /// <param name="doctorOfficeRegistration">The registration details for the new doctor office.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task CreateDoctorOfficeAsync(DoctorOfficeRegistrationDto doctorOfficeRegistration);
+}
---
+++ HealthConnect.Application/Services/DoctorOfficeService.cs
@@ -0,0 +1,92 @@
+﻿namespace HealthConnect.Application.Services;
+
+using AutoMapper;
+using HealthConnect.Application.Dtos.DoctorOffice;
+using HealthConnect.Application.Interfaces;
+using HealthConnect.Application.Interfaces.RepositoriesInterfaces;
+using HealthConnect.Application.Interfaces.ServicesInterface;
+using HealthConnect.Domain.Models;
+
+/// <summary>
+/// Provides handling of Doctor offices business rules for retrieval, creation, update, and deletion.
+/// </summary>
+public class DoctorOfficeService(
+    IDoctorOfficeRepository doctorOfficeRepository,
+    IMapper mapper,
+    IUnitOfWork unitOfWork,
+    IDoctorRepository doctorRepository)
+    : IDoctorOfficeService
+{
+    private readonly IDoctorOfficeRepository _doctorOfficeRepository = doctorOfficeRepository;
+    private readonly IMapper _mapper = mapper;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IDoctorRepository _doctorRepository = doctorRepository;
+
+    /// <inheritdoc/>
+    public async Task<DoctorOfficeSummaryDto?> GetOfficeByIdAsync(Guid id)
+    {
+        var doctorOffice = await _doctorOfficeRepository.GetDoctorOfficeByIdAsync(id) ??
+            throw new NullReferenceException("Doctor office not found.");
+
+        return _mapper.Map<DoctorOfficeSummaryDto>(doctorOffice);
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<DoctorOfficeSummaryDto>> GetAllDoctorOfficeAsync()
+    {
+        var doctorOffices = await _doctorOfficeRepository.GetAllDoctorOfficesAsync();
+        return _mapper.Map<IEnumerable<DoctorOfficeSummaryDto>>(doctorOffices);
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<DoctorOfficeSummaryDto>> GetOfficeByDoctorIdAsync(Guid doctorId)
+    {
+        var doctorOffices = await _doctorOfficeRepository.GetOfficeByDoctorIdAsync(doctorId) ??
+            throw new NullReferenceException("Doctor office not found.");
+
+        return _mapper.Map<IEnumerable<DoctorOfficeSummaryDto>>(doctorOffices);
+    }
+
+    /// <inheritdoc/>
+    public async Task<DoctorOfficeSummaryDto?> GetPrimaryOfficeByDoctorIdAsync(Guid doctorId)
+    {
+        var doctorOffice = await _doctorOfficeRepository.GetPrimaryOfficeByDoctorIdAsync(doctorId) ??
+            throw new NullReferenceException("Doctor primary office not found.");
+
+        return _mapper.Map<DoctorOfficeSummaryDto>(doctorOffice);
+    }
+
+    /// <inheritdoc/>
+    public async Task CreateDoctorOfficeAsync(DoctorOfficeRegistrationDto doctorOfficeRegistration)
+    {
+        if (doctorOfficeRegistration.IsPrimary)
+        {
+            var primaryOfficeExists = await _doctorOfficeRepository.GetPrimaryOfficeByDoctorIdAsync(doctorOfficeRegistration.DoctorId);
+            if (primaryOfficeExists != null)
+            {
+                throw new InvalidOperationException("A primary office already exists for this doctor. Please set the existing primary office to non-primary before assigning a new one.");
+            }
+        }
+
+        var doctor = await _doctorRepository.GetDoctorById(doctorOfficeRegistration.DoctorId) ??
+            throw new NullReferenceException("Doctor not found.");
+
+        var doctorOffice = new DoctorOffice
+        {
+            DoctorId = doctorOfficeRegistration.DoctorId,
+            Street = doctorOfficeRegistration.Street,
+            Number = doctorOfficeRegistration.Number,
+            Complement = doctorOfficeRegistration.Complement,
+            State = doctorOfficeRegistration.State,
+            ZipCode = doctorOfficeRegistration.ZipCode,
+            Phone = doctorOfficeRegistration.Phone,
+            SecretaryPhone = doctorOfficeRegistration.SecretaryPhone,
+            SecretaryEmail = doctorOfficeRegistration.SecretaryEmail,
+            IsPrimary = doctorOfficeRegistration.IsPrimary,
+            Doctor = doctor,
+        };
+
+        await _doctorOfficeRepository.CreateDoctorOfficeAsync(doctorOffice);
+        await _unitOfWork.SaveChangesAsync();
+    }
+}
