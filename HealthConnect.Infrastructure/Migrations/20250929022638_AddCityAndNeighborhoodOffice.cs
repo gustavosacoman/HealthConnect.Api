db9760b970e6b48ec9606bafc41ee0dc410b5626
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-09-28 23:29:10 -0300

    feat: added new migration to city and neighborhood

---
+++ HealthConnect.Infrastructure/Migrations/20250929022638_AddCityAndNeighborhoodOffice.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthConnect.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCityAndNeighborhoodOffice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "DoctorOffices",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Neighborhood",
+                table: "DoctorOffices",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "DoctorOffices");
+
+            migrationBuilder.DropColumn(
+                name: "Neighborhood",
+                table: "DoctorOffices");
+        }
+    }
+}
